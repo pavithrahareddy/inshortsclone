837e4e1419d754ba46e22ffcf2e097913e264ca8
pavithrahareddy <pavithrahareddy@gmail.com>
2021-10-19 11:54:10 +0530

    api done

---
+++ Inshorts-Clone/client/src/components/NavBar.jsx
@@ -0,0 +1,48 @@
+// import { Box, makeStyles, Typography } from '@material-ui/core';
+// import Articles from './components/Articles';
+
+// const useStyles = makeStyles(theme => ({
+//     container: {
+//         background: '#f44336',
+//         color: '#FFFFFF',
+//         display: 'flex',
+//         alignItems: 'center',
+//         height: 48,
+//         marginBottom: 30,
+//         [theme.breakpoints.down('md')]: {
+//             display: 'none'
+//         }
+//     },
+//     logo: {
+//         height: 34,
+//         '&:last-child': {
+//             margin: '0 50px 0 20px'
+//         }
+//     },
+//     text: {
+//         fontSize: 14,
+//         fontWeight: 300,
+//         marginLeft: 50,
+//         fontFamily: "'Roboto',sans-serif",
+//     }
+// }))
+
+// const NavBar = () => {
+//     const classes = useStyles();
+    
+    
+
+//     return (
+//         <Box className={classes.container}>
+//             <Typography className={classes.text}>
+//                 For the best experience use <b>inshorts</b> app on your smartphone
+//             </Typography>
+//             <Box style={{marginLeft: 'auto'}}>
+//                <button onclick="myFunction()">Click me</button>
+                
+//             </Box>
+//         </Box>
+//     )
+// }
+
+// export default NavBar;
