df8cfed4eb68fa2c625e534df64051be13f583b6
pavithrahareddy <pavithrahareddy@gmail.com>
2021-11-12 17:04:07 +0530

    link updated

--- Inshorts-Clone/server/server.js
+++ Inshorts-Clone/server/server.js
@@ -11,6 +11,8 @@ mongoose.connect(
   'mongodb+srv://pavithra:U3XF7zMA2VtXv1G2@muggle-inshorts.ojmqr.mongodb.net/PROJECT0?retryWrites=true&w=majority'
 )
 
+// link
+
 app.use('/', router)
 
 app.listen(3001, function () {
